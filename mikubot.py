48837e44b64cdc6934499fffdd9b7650fa8e1437
BuildTools <unconfigured@null.spigotmc.org>
2025-05-01 18:13:15 -0500

    main

---
+++ mikubot.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands
+import logging
+
+import discord.voice_state
+
+handler = logging.FileHandler(filename='discord.log',encoding='utf-8',mode='w')
+
+discord.Permissions(permissions=274881103872)
+discord.utils.oauth_url(1367576586324414554)
+
+intents = discord.Intents.default()
+intents.message_content= True
+intents.voice_states= True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged on as {bot.user}!')
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    await ctx.send(left+right)
+
+@bot.command()
+async def hello(ctx):
+    sender = ctx.author
+    await ctx.send(f"Hello {sender}!")
+
+@bot.command()
+async def join(ctx):
+    member = ctx.author
+    try:
+        invoice = await member.fetch_voice()
+        await ctx.join()
+    except:
+        await ctx.send(f"{member}, you're not in voice!")
+        
+
+bot.run('MTM2NzU3NjU4NjMyNDQxNDU1NA.GAzwl6.7bU5y3KTZU6B7JYn2oaH7nHy5JyFclDL2sj3b0',log_handler=handler,log_level=logging.DEBUG)
+
+        
+        
+
