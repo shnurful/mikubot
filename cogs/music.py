ccd1fdfde39fdea56cb70795cfcdc1e4f31794db
BuildTools <unconfigured@null.spigotmc.org>
2025-05-02 15:55:23 -0500

    set up play command

--- cogs/music.py
+++ cogs/music.py
@@ -1,24 +1,41 @@
 import discord
+import asyncio
 from discord.ext import commands
 from discord import app_commands
+import yt_dlp
+from yt_dlp import YoutubeDL
+import ffpmpeg
 
 class music(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
+        
+        
+        self.is_playing = False
+        self.is_paused = False
 
-    # @commands.command()
-    # async def play(ctx):
-    #     voice = ctx.voice_client
-    #     try:
-    #         await voice.play()
-    #     except Exception as e:
-    #         if(e == "ClientException"):
-    #             if(not voice.is_connected()):
-    #                 await ctx.send("I'm not in a voice channel")
-    #             else:
-    #                 await ctx.send("I'm busy right now")
-    #         else:
-    #             await ctx.send("Sorry, I can't do that")
+        self.music_queue =[]
+        self.YDL_OPTIONS = {'format' : 'bestaudio', 'noplaylist': 'True'}
+        self.ytdl = yt_dlp.YoutubeDL(self.YDL_OPTIONS)
+        self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnected_streamed 1 -reconnect_delay_max 5', 'options': '-vn -filter:a' "volume-0.25"}
+        self.vc = None
+    
+    @commands.hybrid_command(name="play", description="I'll play the video from the url provided")
+    async def play(self,ctx: commands.Context, url: str):
+        voice = ctx.voice_client
+        try:
+
+            loop = asyncio.get_event_loop()
+
+            data = await loop.run_in_executor(None, lambda: self.ytdl.extract_info(url, download=False))
+            
+            song = data['url']
+            player = discord.FFmpegOpusAudio(song, **self.FFMPEG_OPTIONS)
+
+            voice.play(player)
+
+        except Exception as e:
+            print(e)
 
 
 async def setup(bot: commands.Bot):
