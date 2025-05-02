8dcde4b1ce45dba4f10aab3d8b98e75933ab191e
BuildTools <unconfigured@null.spigotmc.org>
2025-05-01 23:40:42 -0500

    implemented commands as cogs

---
+++ cogs/maincommands.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+class main_commands(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+    
+    #TODO: add exception handling for connecting, improve exception handling for fetch_voice()
+    @commands.command(name="join", description="I will join your current channel")
+    async def join(self,ctx):
+        member = ctx.author
+        try:
+            invoice = await member.fetch_voice()
+            channel = invoice.channel
+        except:
+            await ctx.send(f"{member}, you're not in a channel!")
+
+        await channel.connect()
+    
+    #TODO: add exception handling
+    @commands.command(name="leave", description="I will leave my current channel")
+    async def leave(self,ctx):
+        voice = ctx.voice_client
+        await voice.disconnect()
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(main_commands(bot))
--- mikubot.py
+++ mikubot.py
@@ -1,11 +1,14 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 import logging
+from dotenv import load_dotenv
+import os
 
-import discord.voice_state
-
+load_dotenv()
 handler = logging.FileHandler(filename='discord.log',encoding='utf-8',mode='w')
 
+GUILD_ID = discord.Object(id=1367580756267765851)
 discord.Permissions(permissions=274881103872)
 discord.utils.oauth_url(1367576586324414554)
 
@@ -13,27 +16,35 @@ intents = discord.Intents.default()
 intents.message_content= True
 intents.voice_states= True
 
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix='&', intents=intents)
+
+
+@bot.tree.command(name="load", description="load a cog",guild=GUILD_ID)
+async def load_cog(interaction: discord.Interaction, extension: str):
+    await bot.load_extension(f"cogs.{extension}")
+    await interaction.response.send_message(f"Cog '{extension} loaded.")
+    print(f"Cog '{extension}' has been loaded.")
 
 @bot.event
 async def on_ready():
     print(f'Logged on as {bot.user}!')
+    await bot.load_extension(f"cogs.maincommands")
 
-@bot.command()
-async def join(ctx):
-    member = ctx.author
-    try:
-        invoice = await member.fetch_voice()
-        channel = invoice.channel
-    except:
-        await ctx.send(f"{member}, you're not in a channel!")
 
-    await channel.connect()
 
-@bot.command()
-async def leave(ctx):
-    voice = ctx.voice_client
-    await voice.disconnect()
+# @bot.command()
+# async def play(ctx):
+#     voice = ctx.voice_client
+#     try:
+#         await voice.play()
+#     except Exception as e:
+#         if(e == "ClientException"):
+#             if(not voice.is_connected()):
+#                 await ctx.send("I'm not in a voice channel")
+#             else:
+#                 await ctx.send("I'm busy right now")
+#         else:
+#             await ctx.send("Sorry, I can't do that")
 
 bot.run('MTM2NzU3NjU4NjMyNDQxNDU1NA.GAzwl6.7bU5y3KTZU6B7JYn2oaH7nHy5JyFclDL2sj3b0',log_handler=handler,log_level=logging.DEBUG)
 
