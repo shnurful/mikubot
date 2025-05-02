afd0104808f8eb89cb9bd6967af3734a0e965619
BuildTools <unconfigured@null.spigotmc.org>
2025-05-02 18:03:41 -0500

    added pause, resume, stop

--- cogs/music.py
+++ cogs/music.py
@@ -8,10 +8,6 @@ from yt_dlp import YoutubeDL
 class music(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
-        
-        
-        self.is_playing = False
-        self.is_paused = False
 
         self.music_queue =[]
         self.YDL_OPTIONS = {'format' : 'bestaudio/best', 'noplaylist': 'True'}
@@ -32,9 +28,46 @@ class music(commands.Cog):
 
             voice.play(player)
             await ctx.send(f"Now playing: {url} ")
+            self.is_playing = True
+
         except Exception as e:
             print(e)
 
+    @commands.hybrid_command(name="pause", description="I'll pause what's currently playing")
+    async def pause(self,ctx: commands.Context):
+        voice = ctx.voice_client
+        try:
+            if(voice.is_playing):
+                voice.pause()
+                await ctx.send("Ok! Paused.")
+            else:
+                await ctx.send("I'm not playing anything right now")
+        except Exception as e:
+            print(e)
+    
+    @commands.hybrid_command(name="resume", description="I'll resume what's currently playing")
+    async def resume(self,ctx: commands.Context):
+        voice = ctx.voice_client
+        try:
+            if(voice.is_playing):
+                voice.resume()
+                await ctx.send("Ok! Resuming!")
+            else:
+                await ctx.send("I'm not playing anything right now")
+        except Exception as e:
+            print(e)
+    
+    @commands.hybrid_command(name="stop", description="I'll stop playing")
+    async def resume(self,ctx: commands.Context):
+        voice = ctx.voice_client
+        try:
+            if(voice.is_playing):
+                voice.stop()
+                await ctx.send("Ok! Stopping.")
+            else:
+                await ctx.send("I'm not playing anything right now")
+        except Exception as e:
+            print(e)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(music(bot))
