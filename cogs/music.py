dbbc69b6440e55505fd5c3b9ef052491c2fe7523
morb0 <esilo8@gmail.com>
2025-05-03 19:46:25 -0500

    added autoplay response messages

--- cogs/music.py
+++ cogs/music.py
@@ -198,8 +198,10 @@ class music(commands.Cog):
         
         if not self.autoplay:
             self.autoplay = True
+            ctx.send("Ok! Autoplay enabled.")
         else:
-            self.autoplay = False 
+            self.autoplay = False
+            ctx.send("Ok! Autoplay enabled.") 
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(music(bot))
